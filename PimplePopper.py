7a5435340237719a62efdc4260a6d8ab495bbfa7
andy580 <andy.gehlan@gmail.com>
2020-07-11 18:05:45 -0400

    Displays personal picture as an example

--- PimplePopper.py
+++ PimplePopper.py
@@ -1,2 +1,16 @@
 import cv2
-print("success")
+import math
+import numpy as np
+
+image = cv2.imread("blemish.png")
+
+# Displaying Window     
+windowName = "Blemish Remover"
+cv2.namedWindow(windowName, cv2.WINDOW_AUTOSIZE)
+cv2.imshow(windowName, image)
+
+while True:
+    c = cv2.waitKey(100)
+    if c ==27:
+        break
+cv2.destroyAllWindows();
