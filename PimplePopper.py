85d7afd151f73b11fb253dc7e934da38739aef2d
andy580 <andy.gehlan@gmail.com>
2020-07-12 22:17:47 -0400

    UI to choose picture to edit

--- PimplePopper.py
+++ PimplePopper.py
@@ -1,16 +1,26 @@
 import cv2
 import math
 import numpy as np
+import tkinter
 
-image = cv2.imread("blemish.png")
+# Prevents root window from opening after file is selected
+from tkinter import Tk
+root = Tk()
+root.withdraw()
+# Select image to remove blemish
+from tkinter.filedialog import askopenfilename
+filename = askopenfilename()
+
+image = cv2.imread(filename)
 
 # Displaying Window     
 windowName = "Blemish Remover"
 cv2.namedWindow(windowName, cv2.WINDOW_AUTOSIZE)
 cv2.imshow(windowName, image)
 
+
 while True:
     c = cv2.waitKey(100)
     if c ==27:
         break
-cv2.destroyAllWindows();
+cv2.destroyAllWindows()
