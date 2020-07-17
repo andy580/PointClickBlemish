671e03b3d376a9d4bc468961d85508891790a8cd
andy580 <andy.gehlan@gmail.com>
2020-07-16 23:44:24 -0400

    Blemish removed through weighted add operation

--- PimplePopper.py
+++ PimplePopper.py
@@ -10,12 +10,14 @@ root.withdraw()
 # Select image to remove blemish
 from tkinter.filedialog import askopenfilename
 filename = askopenfilename()
-
+ 
 image = cv2.imread(filename)
-
+output = image.copy()
 
 # Function to select and replace skin patch
 def blemishRemover(action, x, y, flags, userdata):
+    global image
+    global output
     # Selecting blemish: set temporary patch to 16x16 pixels
     if action == cv2.EVENT_LBUTTONDOWN:
         blemish = image[y-8:y+8, x-8:x+8].copy()
@@ -33,7 +35,7 @@ def blemishRemover(action, x, y, flags, userdata):
 
         # Look for adjacent skin patches to replace blemish
         # Adjacent skin patch with the most similar boundary selected
-        for theta in range(0, 360, 90):
+        for theta in range(0, 360, 5):
             xoff = int(distFromBlemish * math.cos(np.deg2rad(theta)))+x
             yoff = int(distFromBlemish * math.sin(np.deg2rad(theta)))+y
 
@@ -65,11 +67,40 @@ def blemishRemover(action, x, y, flags, userdata):
             optimalDict[theta] = boundaryDict[theta]/maxBoundary + smoothnessDict[theta]/maxSmoothness
         optimalTheta = min(optimalDict, key=optimalDict.get)    
 
+        # Seamlessly clone best skin patch onto blemish
+        xoff = int(distFromBlemish * math.cos(np.deg2rad(optimalTheta)))+x
+        yoff = int(distFromBlemish * math.sin(np.deg2rad(optimalTheta)))+y
+        bestSkinPatch = image[yoff-8:yoff+8, xoff-8:xoff+8].copy()
+
+        # mask = np.ones(bestSkinPatch.shape[0:2], bestSkinPatch.dtype)*255
+        mask = np.ones((16,16), float)
+        # print(mask)
+        for i in range(5,-1,-1):
+            mask[:,i] = mask[i,:] = mask[:,-(i+1)] = mask[-(i+1),:] = (i/5)
+            
+        maskInv = 1-mask
+        patch = image[y-8:y+8, x-8:x+8]
+        
+        mask = cv2.merge((mask,mask,mask))
+        maskInv = cv2.merge((maskInv,maskInv,maskInv))
+
+        bestSkinPatch = np.uint8(bestSkinPatch*mask)
+        patch = np.uint8(patch*maskInv)
+        
+        outputPatch = bestSkinPatch + patch
+
+        output = cv2.addWeighted(image[y-8:y+8, x-8:x+8],0.2, outputPatch,0.8,0.0)
+        image[y-8:y+8, x-8:x+8] = output
+        cv2.medianBlur(image[y-20:y+20, x-20:x+20],5)
+
+        cv2.imshow(windowName, image)
+
 
 # Displaying Window     
 windowName = "Blemish Remover"
-cv2.namedWindow(windowName, cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 cv2.imshow(windowName, image)
+# cv2.imshow("output", bestSkinPatch)
 cv2.setMouseCallback(windowName, blemishRemover)
 
 
